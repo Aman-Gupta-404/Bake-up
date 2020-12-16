e164234d9847bee67115d9292a838d774c69ad0b
Aman-Gupta-404 <ag9139563@gmail.com>
2020-12-16 10:32:18 +0530

    make it better

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 // database connection
 mongoose
-  .connect(process.env.MONGODB_URI || process.env.DB_URI, {
+  .connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
